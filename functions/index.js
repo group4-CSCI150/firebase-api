e2daae92c235afb198d7231b83abb29891889d00
Leonardo Yoshida <yoshida.leoy@gmail.com>
2019-10-24 14:58:25 -0700

    add cors

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,15 @@
 
 const functions = require("firebase-functions")
 const express = require("express");
+const cors = require('cors');
+
 //const organizationRouter = require('./routes/organization')
 const userRouter = require('./routes/user')
 
 /* Express */
 const app = express();
 
+app.use(cors({ origin: true }))
 app.use('/user', userRouter)
 //app.use('/organization', organizationRouter)
 
