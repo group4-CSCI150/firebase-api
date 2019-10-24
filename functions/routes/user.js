77957bedf3503e4cbafd846e268d1f2ac0746b7f
Leonardo Yoshida <yoshida.leoy@gmail.com>
2019-10-24 16:52:34 -0700

    remove commented code and fix get all users

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -7,19 +7,19 @@ router.route('/')
     .post((req, res) => {
         var doc = users;
         if (req.body)
-        doc.doc(req.body.username).set(req.body)
-            .then(user => {
-                return res.status(200).json({ "id": user.id, message: "USER CREATED" });
-            }).catch((error) => {
-                return res.status(400).json({ "message": "Unable to connect to Firestore. USER" });
-            });
+            doc.doc(req.body.username).set(req.body)
+                .then(user => {
+                    return res.status(200).json({ "id": user.id, message: "USER CREATED" });
+                }).catch((error) => {
+                    return res.status(400).json({ "message": "Unable to connect to Firestore. USER" });
+                });
     })
     .get((req, res) => {
-        var doc = users.orderBy("name");
-        var all = {'users' : []};
+        var doc = users
+        var all = { 'users': [] };
         doc.get().then(users => {
             users.forEach(element => {
-                all['users'].push({"id": element.id,  "user": element.data(), message: "ALL users returned" });
+                all['users'].push({ "id": element.id, "user": element.data(), message: "ALL users returned" });
             });
             return res.status(200).json(all);
         }).catch((error) => {
@@ -43,38 +43,20 @@ router.route('/byID/:userId')
     .put((req, res) => {
         var doc = users.doc(req.params.userId);
         doc.update(req.body).then(() => {
-            return res.status(200).json({"message": "Successful update"});
+            return res.status(200).json({ "message": "Successful update" });
         }).catch(() => {
-            return res.status(404).json({"message": "Fail"});
+            return res.status(404).json({ "message": "Fail" });
         })
     })
     .delete((req, res) => {
         var doc = users.doc(req.params.userId);
         doc.delete().then(() => {
-            return res.status(200).json({"message": "Successful Delete"});
+            return res.status(200).json({ "message": "Successful Delete" });
         }).catch(() => {
-            return res.status(404).json({"message": "Fail"});
+            return res.status(404).json({ "message": "Fail" });
         })
     })
-/*
-router.route('/find/:username')
-    .get((req, res) => {
-        var doc = users.where("name", "=" ,req.params.username);
-        var all = {'users' : []};
-        doc.get().then(users => {
-            users.forEach(element => {
-                all['users'].push({"id": element.id,  "user": element.data() });
-            });
-            if (all['users'].isEmpty())
-            {
-                return res.status(200).json({"message": "No user found. USER"});
-            }
-            return res.status(200).json(all);
-        }).catch((error) => {
-            return res.status(400).json({ "message": "Unable to connect to Firestore. USER" });
-        });
-    })
-*/
+
 router.route('/login')
     .post((req, res) => {
         var doc = users.doc(req.body.username);
