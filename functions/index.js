4c8e40d6fa797e427dce677d947ae1580a17d756
brandonkns66 <51934408+brandonkns66@users.noreply.github.com>
2019-12-03 15:04:51 -0800

    Added chat api endpoint

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 
 //const organizationRouter = require('./routes/organization')
 const userRouter = require('./routes/user')
+const chatRouter = require('./routes/chat')
 
 /* Express */
 const app = express();
 
 app.use(cors({ origin: true }))
 app.use('/user', userRouter)
+app.use('/chat', chatRouter)
 //app.use('/organization', organizationRouter)
 
 const api = functions.https.onRequest(app)
